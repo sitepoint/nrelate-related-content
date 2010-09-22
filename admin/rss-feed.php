f4e1ba1dd41674679178ce3a0f49090373c80502
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-22 01:30:17 +0000

    v0.40.2: remove_all_filters for get_the_permalink in the nrelate custom rss feed.
    git-svn-id: https://plugins.svn.wordpress.org/nrelate-related-content/tags/0.40.2@292228 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/rss-feed.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * nrelate custom RSS feed
+ *
+ * @package nrelate
+ * @subpackage Functions
+ */
+
+// Set up a unique nrelate key, for secure feed access
+	$key = get_option( 'nrelate_key' );
+	if ( empty( $key ) ) {
+		$key = wp_generate_password( 24, false, false );
+		update_option( 'nrelate_key', $key );
+		add_defaults_nr_rc(); // Send the key to nrelate
+	}
+	
+/**
+ * Serve a custom full-text feed. Thwarts FeedBurner plugins
+ */
+function nrelate_custom_feed() {
+	if ( isset( $_GET['nrelate_feed'] ) && $_GET['nrelate_feed'] == get_option( 'nrelate_key' ) ) {
+		if ( isset( $_GET['posts_per_page'] ) )
+			set_query_var( 'posts_per_page', $_GET['posts_per_page'] );
+		else
+			set_query_var( 'posts_per_page', 50 );
+		// Query the posts. Defaults to 50, but we can override
+		query_posts(
+			array(
+				'posts_per_page' => get_query_var( 'posts_per_page' ),
+				'paged' => get_query_var( 'paged' )
+			)
+		);
+		// Force the feed to return full content
+		add_filter( 'pre_option_rss_use_excerpt', create_function( '', 'return 0;' ) );
+		
+		// Remove all filters from the_permalink_rss
+		// some plugins are filtering this function and causing problems
+		remove_all_filters ('the_permalink_rss');	
+		
+		
+		// Use WP's feed template
+		do_feed_rss2( false );
+		exit();
+	}
+}
+add_action( 'template_redirect', 'nrelate_custom_feed', 5 );
+?>
