f51d3e78087981ba369072f4c83eb5a6fb7e1ba0
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-03 15:36:06 +0000

    version 0.42.1
    git-svn-id: https://plugins.svn.wordpress.org/nrelate-related-content/tags/0.42.1@307400 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin-messages.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * nrelate Admin Messages
+ *
+ * Does system checks and sets messages for this particular nrelate plugin
+ *
+ * @package nrelate
+ * @subpackage Functions
+ */
+
+function nr_admin_message_set(){
+	
+	// Let's write some messages
+	// Simple create div with id adverify for nrelate to populate the content
+		$msg = $msg . '<li id="adverify"></li>';
+
+	echo $msg;
+};
+add_action ('nrelate_admin_messages','nr_admin_message_set');
+
+
+		
+?>
